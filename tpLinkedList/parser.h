e9cb3e94ba1f621d5bdc3d4a90f05b5b5cd6612e
srv90 <52800820+srv90@users.noreply.github.com>
2021-06-21 13:10:22 -0300

    Delete tpLinkedList directory

--- tpLinkedList/parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_H_INCLUDED
-#define PARSER_H_INCLUDED
-
-
-
-#endif // PARSER_H_INCLUDED
-
-int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee);
-int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee);
