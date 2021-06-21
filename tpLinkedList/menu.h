ec298a0799dd822e49a1fd004bde8cf84410cd75
srv90 <52800820+srv90@users.noreply.github.com>
2021-06-20 22:45:19 -0300

    Add files via upload

---
+++ tpLinkedList/menu.h
@@ -0,0 +1,24 @@
+/*
+ * menu.h
+ *
+ *  Created on: 13 jun. 2021
+ *      Author: Quique
+ */
+
+#ifndef MENU_H_
+#define MENU_H_
+
+
+
+#endif /* MENU_H_ */
+
+
+/** \brief returns the option selected by the user
+ *
+ * \return int
+ */
+int menu();
+
+int menuModificacion();
+
+
